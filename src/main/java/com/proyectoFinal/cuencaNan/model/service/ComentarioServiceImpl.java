4db30c2ec2651d025b38916b6593e200c58ef529
Juanc Idrovo <juancidrovo@gmail.com>
2024-05-29 17:42:17 -0500

    error
    error corregido...

--- src/main/java/com/proyectoFinal/cuencaNan/model/service/ComentarioServiceImpl.java
+++ src/main/java/com/proyectoFinal/cuencaNan/model/service/ComentarioServiceImpl.java
@@ -1,13 +1,14 @@
 package com.proyectoFinal.cuencaNan.model.service;
 
 import java.util.List;
-
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
-
 import com.proyectoFinal.cuencaNan.model.dao.IComentarioDao;
 import com.proyectoFinal.cuencaNan.model.entity.Comentario;
 
+
+@Service
 public class ComentarioServiceImpl implements IComentarioService {
 	
 	@Autowired
@@ -16,28 +17,28 @@ public class ComentarioServiceImpl implements IComentarioService {
 	@Override
 	@Transactional(readOnly = true)
 	public List<Comentario> findAll() {
-		// TODO Auto-generated method stub
+		
 		return (List<Comentario>) comenDao.findAll();
 	}
 
 	@Override
 	@Transactional
 	public Comentario save(Comentario comentario) {
-		// TODO Auto-generated method stub
+		
 		return comenDao.save(comentario);
 	}
 
 	@Override
 	@Transactional(readOnly = true)
 	public Comentario findById(Long idComentario) {
-		// TODO Auto-generated method stub
+	
 		return comenDao.findById(idComentario).orElse(null);
 	}
 
 	@Override
 	@Transactional
 	public void delete(Long idComentario) {
-		// TODO Auto-generated method stub
+		
 		comenDao.deleteById(idComentario);
 	}
 
