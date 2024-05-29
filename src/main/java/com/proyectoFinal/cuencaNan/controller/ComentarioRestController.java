02c8898097b9b782c52dd4d8886215092f8679f3
Juanc Idrovo <juancidrovo@gmail.com>
2024-05-29 09:07:44 -0500

    correccion comentario
    creo q ahi ta

--- src/main/java/com/proyectoFinal/cuencaNan/controller/ComentarioRestController.java
+++ src/main/java/com/proyectoFinal/cuencaNan/controller/ComentarioRestController.java
@@ -14,51 +14,50 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
 
-import com.proyectoFinal.cuencaNan.model.dao.IComentarioDao;
 import com.proyectoFinal.cuencaNan.model.entity.Comentario;
-import com.proyectoFinal.cuencaNan.model.entity.Usuario;
+
 import com.proyectoFinal.cuencaNan.model.service.IComentarioService;
 
 @RestController
 @RequestMapping("/api")
 public class ComentarioRestController {
-	
+
 	@Autowired
 	private IComentarioService comenServe;
-	
+
 	// listar_todas_los_usuarios
-		@GetMapping("/listar")
-		public List<Comentario> indext() {
-			return comenServe.findAll();
-		}
+	@GetMapping("/comentarios")
+	public List<Comentario> indext() {
+		return comenServe.findAll();
+	}
 
-		// buscar_un usuario_por_id
-		@GetMapping("/buscar/{id}")
-		public Comentario show(@PathVariable Long idComentario) {
-			return comenServe.findById(idComentario);
-		}
+	// buscar_un usuario_por_id
+	@GetMapping("/buscar/{idComentario}")
+	public Comentario show(@PathVariable Long idComentario) {
+		return comenServe.findById(idComentario);
+	}
 
-		// guardar_un_usuario
-		@PostMapping("/guardar")
-		@ResponseStatus(HttpStatus.CREATED)
-		public Comentario create(@RequestBody Comentario comentario) {
-			return comenServe.save(comentario);
-		}
+	// guardar_un_usuario
+	@PostMapping("/guardar")
+	@ResponseStatus(HttpStatus.CREATED)
+	public Comentario create(@RequestBody Comentario comentario) {
+		return comenServe.save(comentario);
+	}
 
-		// editar_un_usuario
-		@PutMapping("/editar/{idComentario}")
-		@ResponseStatus(HttpStatus.CREATED)
-		public Comentario update(@RequestBody Comentario comentario, @PathVariable Long idComentario) {
-			Comentario comentarioActual = comenServe.findById(idComentario);
-			comentarioActual.setComentario(comentario.getComentario());
-			return comenServe.save(comentarioActual);
-		}
+	// editar_un_usuario
+	@PutMapping("/editar/{idComentario}")
+	@ResponseStatus(HttpStatus.CREATED)
+	public Comentario update(@RequestBody Comentario comentario, @PathVariable Long idComentario) {
+		Comentario comentarioActual = comenServe.findById(idComentario);
+		comentarioActual.setComentario(comentario.getComentario());
+		return comenServe.save(comentarioActual);
+	}
 
-		// eliminar_un_usuario
-		@DeleteMapping("/eliminar/{idComentario}")
-		@ResponseStatus(HttpStatus.NO_CONTENT)
-		public void delete(@PathVariable Long idComentario) {
-			comenServe.delete(idComentario);
-		}
+	// eliminar_un_usuario
+	@DeleteMapping("/eliminar/{idComentario}")
+	@ResponseStatus(HttpStatus.NO_CONTENT)
+	public void delete(@PathVariable Long idComentario) {
+		comenServe.delete(idComentario);
+	}
 
 }
