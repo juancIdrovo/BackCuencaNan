9bfc649fbaa6443a60f69e12cd576b6bbd408cc2
Franklin Orellana <franklin0994324757@gmail.com>
2024-06-03 10:24:17 -0500

    Revert "Persona"
    This reverts commit cfc6e8193edc33f69e5be63c67ee012f37bda574.

--- src/main/java/com/proyectoFinal/cuencaNan/model/entity/Administrador.java
+++ src/main/java/com/proyectoFinal/cuencaNan/model/entity/Administrador.java
@@ -11,7 +11,7 @@ import jakarta.persistence.UniqueConstraint;
 
 @Entity
 @Table(name = "administradores", uniqueConstraints = { @UniqueConstraint(columnNames = { "cedula" }) })
-public class Administrador  extends Persona {
+public class Administrador implements Serializable {
 
 	private static final long serialVersionUID = 1L;
 	@Id
