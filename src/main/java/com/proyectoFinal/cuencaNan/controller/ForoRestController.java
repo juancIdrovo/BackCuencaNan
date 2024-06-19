0d13b59eb75b5a14bf19648f81b1e658e0a89205
Franklin Orellana <franklin0994324757@gmail.com>
2024-06-19 08:20:14 -0500

    CrossOrigin

--- src/main/java/com/proyectoFinal/cuencaNan/controller/ForoRestController.java
+++ src/main/java/com/proyectoFinal/cuencaNan/controller/ForoRestController.java
@@ -4,6 +4,7 @@ import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -16,6 +17,7 @@ import org.springframework.web.bind.annotation.RestController;
 import com.proyectoFinal.cuencaNan.model.entity.Foro;
 import com.proyectoFinal.cuencaNan.model.service.IForoService;
 
+@CrossOrigin(origins= {"http://localhost:4200"})
 @RestController
 @RequestMapping("/api")
 public class ForoRestController {
--- src/main/java/com/proyectoFinal/cuencaNan/controller/TipoEventosRestController.java
+++ src/main/java/com/proyectoFinal/cuencaNan/controller/TipoEventosRestController.java
@@ -4,6 +4,7 @@ import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -18,6 +19,7 @@ import com.proyectoFinal.cuencaNan.model.entity.Administrador;
 import com.proyectoFinal.cuencaNan.model.entity.TiposEventos;
 import com.proyectoFinal.cuencaNan.model.service.ITipoEventosService;
 
+@CrossOrigin(origins= {"http://localhost:4200"})
 @RestController
 @RequestMapping("/api")
 public class TipoEventosRestController {
